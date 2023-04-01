36823dede48975d45bb149ca44ef1d7d515bfa31
upasanasaha <upasanasaha40@gmail.com>
2023-04-01 23:24:49 +0530

    Updated project.

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ headers = {
 }
 
 try:
-    mongoDB_url=os.getenv("MONGO_DB_URL")
+    mongoDB_url='mongodb+srv://UpasanaSaha:11$Upcellent@cluster0.7hozwpx.mongodb.net/?retryWrites=true&w=majority'
     api_key = 'AIzaSyAfaCUAHOR6eGTV8E5LwKV9h31PZyjAaH4'
     Youtube_Scraper = build('youtube', 'v3', developerKey=api_key)
     #mydb, cursor = get_database() cant use because heroku will not support
